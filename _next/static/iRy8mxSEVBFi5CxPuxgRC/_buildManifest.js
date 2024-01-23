6a85257c9faaee499a186618b7e31a75f14814ad
TheJewGamer <jacobcohen@duck.com>
2024-01-23 07:20:12 +0000

    Deploying to site from @ TheJewGamer/ValorantStratRoulette@c559165bc9705e9295cdd20ce97fcba027471e54 🚀

--- _next/static/iRy8mxSEVBFi5CxPuxgRC/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/8a5d14635c8f0d5e.css","static/chunks/pages/index-e63856854631bbd2.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tPEbRuVSKzZMT86FyWcyg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/30bdf92437ba5731.css","static/chunks/pages/index-76da5d75894817db.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
